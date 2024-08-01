0aefb3c1b57eea819b5653fea6a4038e4d953632
kozinus <kkozin2403@gmail.com>
2024-08-01 04:40:10 +0300

    Сортировки [#505155]

---
+++ src/main/java/ru/job4j/algo/IntervalMerger.java
@@ -0,0 +1,26 @@
+package ru.job4j.algo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class IntervalMerger {
+
+    public int[][] merge(int[][] intervals) {
+        int[][] result = new int[][] {};
+        int pointer = 0;
+        if (intervals.length > 0) {
+            Arrays.sort(intervals, Comparator.comparingInt(x -> x[0]));
+            result = new int[intervals.length][2];
+            result[0] = intervals[0];
+            for (int i = 1; i < intervals.length; i++) {
+                if (result[pointer][1] >= intervals[i][0]) {
+                    result[pointer][1] = intervals[i][1];
+                } else {
+                    pointer++;
+                    result[pointer] = intervals[i];
+                }
+            }
+        }
+        return Arrays.copyOf(result, pointer + 1);
+    }
+}
